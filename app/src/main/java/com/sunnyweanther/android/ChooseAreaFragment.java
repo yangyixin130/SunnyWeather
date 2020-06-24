2ad2a884f27dd07cffb383a0c89729d2185891d1
yangyixin130 <1578111082@qq.com>
2020-06-25 07:04:00 +0800

    实现自动跟新和切换城市

--- app/src/main/java/com/sunnyweanther/android/ChooseAreaFragment.java
+++ app/src/main/java/com/sunnyweanther/android/ChooseAreaFragment.java
@@ -106,6 +106,12 @@ public class ChooseAreaFragment extends Fragment {
                         startActivity(intent);
                         getActivity().finish();
                     }
+                    else if(getActivity() instanceof WeatherActivity){
+                        WeatherActivity activity = (WeatherActivity) getActivity();
+                        activity.drawerLayout.closeDrawers();
+                        activity.swipeRefresh.setRefreshing(true);
+                        activity.requestWeather(weatherId);
+                    }
                 }
 
             }
